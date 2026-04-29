d29dfe1c349268a71de0aa7482871ed55f183ec7
Marcelo Wiebbelling <mrodrigow@gmail.com>
2026-04-29 21:56:21 +0200

    Removing frontend

--- frontend/components/oncall/IncidentTable.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="text-sm text-gray-500">
-    <!-- Incident list is rendered inline in the period detail page -->
-    <slot />
-  </div>
-</template>
-
-<script setup lang="ts"></script>
